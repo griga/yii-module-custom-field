669b76b2fd13504ff0ef23917eb7490f9d37aba2
Yura Griga <grigach@gmail.com>
2014-08-12 22:44:59 +0300

    progress

---
+++ controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+/** Created by griga at 12.08.14 | 22:40.
+ * 
+ */
+
+class AdminController extends BackendController{
+
+    /**
+     *
+     */
+    public function actionIndex()
+    {
+        $this->render('admin');
+    }
+
+} 
